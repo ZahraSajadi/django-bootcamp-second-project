5bb713f3d1af4d5c5939af5f7872c9d684bf4d89
hossein-mo <hossein.mo.c@gmail.com>
2024-03-04 10:19:41 +0100

    fixed errors

--- reservation/models.py
+++ reservation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class Room(models.Model):
     name = models.CharField(max_length=100)
     capacity = models.PositiveIntegerField()
@@ -9,28 +10,28 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Reservation(models.Model):
-    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
-    team = models.ForeignKey('Team', on_delete=models.CASCADE, null=True)
+    team = models.ForeignKey("users.Team", on_delete=models.CASCADE, null=True)
     date = models.DateField()
     time = models.TimeField()
     note = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f'Reservation for {self.user.team.name} on {self.date} at {self.time}'
-    
+        return f"Reservation for {self.user.team.name} on {self.date} at {self.time}"
+
 
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
-    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
 
 
 class Rating(models.Model):
     value = models.IntegerField()
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
-    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
