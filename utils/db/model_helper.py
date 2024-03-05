aec4052eda3b3eaeb0bc9319b61babc65405eb76
ZahraSajadi <sajady.it@gmail.com>
2024-03-06 01:35:33 +0330

    Add phone_regex, Rename and implement random_password method

--- utils/db/model_helper.py
+++ utils/db/model_helper.py
@@ -1,2 +1,12 @@
-def random_password():
-    pass
+import random
+from django.core.validators import RegexValidator
+
+phone_regex = RegexValidator(
+    regex=r"^09\d{9}$",
+    message="The phone number is wrong. The phone number must be 11 digits and start with 09",
+)
+
+
+def generate_otp(length=6):
+    otp = "".join(random.choices("0123456789", k=length))
+    return otp
