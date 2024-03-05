a80c4f257a5dde7a60092db38455204985408630
ZahraSajadi <sajady.it@gmail.com>
2024-03-06 01:34:18 +0330

    Update test files

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,56 +1,49 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
-from users.models import Team, OTP, CustomUserManager
+from users.models import Team, OTP
 
 User = get_user_model()
-
-
-class CustomUserManagerTestCase(TestCase):
-    def setUp(self):
-        self.manager = CustomUserManager()
-
-    def test_create_user_with_email(self):
-        user = self.manager.create_user(
-            email="test@example.com", first_name="John", last_name="Doe", team=None
-        )
-
-        self.assertIsNotNone(user, "User should be created successfully")
-
-        self.assertEqual(
-            user.email, "test@example.com", "User email should be set correctly"
-        )
-
-        self.assertEqual(
-            user.first_name, "John", "User first name should be set correctly"
-        )
-        self.assertEqual(
-            user.last_name, "Doe", "User last name should be set correctly"
-        )
-
-        self.assertFalse(user.has_usable_password(), "User password should not be set")
-
-    def test_create_user_missing_email(self):
-        with self.assertRaises(ValueError):
-            self.manager.create_user(email="", first_name="Jane", last_name="Smith")
 
 
 class CustomUserModelTest(TestCase):
     def setUp(self):
         self.team = Team.objects.create(name="Test Team")
-
-    def test_create_custom_user(self):
-        user = User.objects.create_user(
+        self.user = User.objects.create_user(
+            username="TestUser",
             email="test@example.com",
             first_name="John",
             last_name="Doe",
             phone="09123456789",
             team=self.team,
         )
-        self.assertEqual(user.email, "test@example.com")
-        self.assertEqual(user.first_name, "John")
-        self.assertEqual(user.last_name, "Doe")
-        self.assertEqual(user.phone, "09123456789")
-        self.assertEqual(user.team, self.team)
+
+    def test_create_custom_user(self):
+        self.assertEqual(self.user.username, "TestUser")
+        self.assertEqual(self.user.email, "test@example.com")
+        self.assertEqual(self.user.first_name, "John")
+        self.assertEqual(self.user.last_name, "Doe")
+        self.assertEqual(self.user.phone, "09123456789")
+        self.assertEqual(self.user.team, self.team)
+
+    def test_phone_number_validation(self):
+        # Test valid phone number
+        self.user.full_clean()  # Validate the model instance
+
+        # Test invalid phone number (less than 11 digits)
+        with self.assertRaises(ValidationError):
+            User(username="testuser", phone="0912345678", team=self.team).full_clean()
+
+        # Test invalid phone number (more than 11 digits)
+        with self.assertRaises(ValidationError):
+            User(username="testuser", phone="091234567890", team=self.team).full_clean()
+
+        # Test invalid phone number (does not start with 09)
+        with self.assertRaises(ValidationError):
+            User(username="testuser", phone="08123456789", team=self.team).full_clean()
+
+    def test_user_str_representation(self):
+        self.assertEqual(str(self.user), "John Doe")
 
 
 class TeamModelTest(TestCase):
@@ -63,6 +56,7 @@ class OTPModelTestCase(TestCase):
     def setUp(self):
         # Create a user for testing
         self.user = get_user_model().objects.create_user(
+            username="Test User",
             email="test@example.com",
             first_name="John",
             last_name="Doe",
@@ -74,9 +68,15 @@ class OTPModelTestCase(TestCase):
         self.otp = OTP.objects.create(user=self.user, otp="123456")
 
     def test_otp_model(self):
-        # Retrieve the OTP instance created in setUp
-        otp = OTP.objects.get(user=self.user)
+        otp = OTP.objects.filter(user=self.user).order_by("-created_at").first()
 
         # Check if the OTP instance was created successfully
         self.assertEqual(otp.otp, "123456")
         self.assertEqual(otp.user, self.user)
+
+    def test_otp_generation(self):
+        otp_instance = OTP(user=self.user)
+        otp_instance.save()
+
+        self.assertEqual(len(otp_instance.otp), 6)  # Check OTP length
+        self.assertTrue(otp_instance.otp.isdigit())  # Check OTP is numeric
