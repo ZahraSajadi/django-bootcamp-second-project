c5679b14f918c888634bfd056e2b743cabc2de10
ZahraSajadi <sajady.it@gmail.com>
2024-03-06 05:26:05 +0330

    Create UserAdmin and TeamAdmin models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from .models import Team
+
+
+@admin.register(get_user_model())
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email"]
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
