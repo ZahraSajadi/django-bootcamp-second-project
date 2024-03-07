e7639cd254743bcdb55a3361ec499aa3eda1f54b
hossein-mo <hossein.mo.c@gmail.com>
2024-03-07 11:55:55 +0100

    [feat] added home app

---
+++ home/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render  # noqa: F401
+
+
+class HomeView: ...
--- second_project/urls.py
+++ second_project/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("users.urls")),
     path("room/", include("reservation.urls")),
+    path("home/", include("home.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
