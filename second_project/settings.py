27c0efd55a5823ad3a7406f96ee393f85765e8f8
Mojtaba Ghasemi <mghasemi13786@gmail.com>
2024-03-07 19:30:36 +0330

    [stag] add some tips

--- second_project/settings.py
+++ second_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "second_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / 'statics']
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- second_project/urls.py
+++ second_project/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("user/", include("users.urls")),
-    path("room/", include("reservation.urls")),
-    path("home/", include("home.urls")),
-    path("admins/", include("admins.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path("admin/", admin.site.urls),
+                  path("", TemplateView.as_view(), name='index'),
+                  path("user/", include("users.urls")),
+                  path("room/", include("reservation.urls")),
+                  path("home/", include("home.urls")),
+                  path("admins/", include("admins.urls")),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
