5bea8b227b63bb0b9ec8c07002f3081d680c911a
hossein-mo <hossein.mo.c@gmail.com>
2024-03-07 14:00:08 +0100

    [feat] added view list admin permissions

--- utils/management/commands/create_groups_and_permissions.py
+++ utils/management/commands/create_groups_and_permissions.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
-from reservation.models import Reservation
+from reservation.models import Reservation, Room
+from users.models import Team
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
@@ -9,25 +13,52 @@ class Command(BaseCommand):
         team_leaders_group, created = Group.objects.get_or_create(name="Team Leaders")
         admins_group, created = Group.objects.get_or_create(name="Admins")
 
-        reservation_content_type = ContentType.objects.get_for_model(Reservation)
+        # Team Leaders
+        content_type = ContentType.objects.get_for_model(Reservation)
         add_reservation_self_team, created = Permission.objects.get_or_create(
             name="Can add reservation to their team",
             codename="add_reservation_self_team",
-            content_type=reservation_content_type,
+            content_type=content_type,
         )
         change_reservation_self_team, created = Permission.objects.get_or_create(
             name="Can change reservation of their team",
             codename="change_reservation_self_team",
-            content_type=reservation_content_type,
+            content_type=content_type,
         )
         delete_reservation_self_team, created = Permission.objects.get_or_create(
             name="Can delete reservation of their team",
             codename="delete_reservation_self_team",
-            content_type=reservation_content_type,
+            content_type=content_type,
         )
         team_leaders_group.permissions.add(add_reservation_self_team)
         team_leaders_group.permissions.add(change_reservation_self_team)
         team_leaders_group.permissions.add(delete_reservation_self_team)
+
+        # Admins
+        content_type = ContentType.objects.get_for_model(Room)
+        view_room_list, created = Permission.objects.get_or_create(
+            name="Can view rooms list",
+            codename="view_room_list",
+            content_type=content_type,
+        )
+        content_type = ContentType.objects.get_for_model(Reservation)
+        view_reservation_list, created = Permission.objects.get_or_create(
+            name="Can view reservations list",
+            codename="view_reservation_list",
+            content_type=content_type,
+        )
+        content_type = ContentType.objects.get_for_model(User)
+        view_user_list, created = Permission.objects.get_or_create(
+            name="Can view users list",
+            codename="view_user_list",
+            content_type=content_type,
+        )
+        content_type = ContentType.objects.get_for_model(Team)
+        view_team_list, created = Permission.objects.get_or_create(
+            name="Can view teams list",
+            codename="view_team_list",
+            content_type=content_type,
+        )
 
         can_add_team = Permission.objects.get(codename="add_team")
         can_change_team = Permission.objects.get(codename="change_team")
@@ -46,6 +77,10 @@ class Command(BaseCommand):
         can_view_customuser = Permission.objects.get(codename="view_customuser")
         can_change_customuser = Permission.objects.get(codename="change_customuser")
 
+        admins_group.permissions.add(view_room_list)
+        admins_group.permissions.add(view_reservation_list)
+        admins_group.permissions.add(view_user_list)
+        admins_group.permissions.add(view_team_list)
         admins_group.permissions.add(can_add_team)
         admins_group.permissions.add(can_change_team)
         admins_group.permissions.add(can_delete_team)
