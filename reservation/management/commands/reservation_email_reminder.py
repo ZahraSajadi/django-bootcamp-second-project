990584765ec5d7477497ba48758cb85e6a9073f0
hossein-mo <hossein.mo.c@gmail.com>
2024-03-10 23:03:25 +0100

    added cron job to settings.py

--- reservation/management/commands/reservation_email_reminder.py
+++ reservation/management/commands/reservation_email_reminder.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from typing import Any
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
@@ -13,16 +11,15 @@ User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> str | None:
-        timezone.now()
-
-        reservations = Reservation.objects.filter(start_date__lte=timezone.now() + timezone.timedelta(hours=1))
-
+        reservations = Reservation.objects.filter(start_date__lte=timezone.now() + timezone.timedelta(hours=2))
         for reservation in reservations:
             team = reservation.team
             users = User.objects.filter(team=team)
 
             for user in users:
                 subject = "Meeting time reminder..."
-                message = f"{user.first_name} {user.last_name}, please note, reservation for your team's meeting will be held at {reservation.start_date}"
+                message = f"""{user.first_name} {user.last_name}, please note,
+                reservation for your team's meeting will be held
+                from {reservation.start_date} to {reservation.end_date} in {reservation.room}"""
                 recipient = user.email
-                send_mail(message, subject, recipient)
+                send_mail(subject, message, recipient_list=(recipient,), from_email="noreply@unchained.com")
