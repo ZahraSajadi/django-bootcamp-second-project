620fbc4bbe4d53f9ae0f09cab4f44e2f4bdd4500
hossein-mo <hossein.mo.c@gmail.com>
2024-03-11 11:57:58 +0100

    added test cases for email reminder command

--- reservation/management/commands/reservation_email_reminder.py
+++ reservation/management/commands/reservation_email_reminder.py
@@ -15,11 +15,17 @@ class Command(BaseCommand):
         for reservation in reservations:
             team = reservation.team
             users = User.objects.filter(team=team)
-
+            email_sent = 0
             for user in users:
                 subject = "Meeting time reminder..."
                 message = f"""{user.first_name} {user.last_name}, please note,
                 reservation for your team's meeting will be held
                 from {reservation.start_date} to {reservation.end_date} in {reservation.room}"""
                 recipient = user.email
-                send_mail(subject, message, recipient_list=(recipient,), from_email="noreply@unchained.com")
+                email_sent += send_mail(
+                    subject,
+                    message,
+                    recipient_list=(recipient,),
+                    from_email="noreply@unchained.com",
+                )
+            print(str(email_sent) + " reminder email sent.")
---
+++ reservation/tests/test_commands.py
@@ -0,0 +1,43 @@
+from io import StringIO
+import sys
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils import timezone
+from reservation.models import Reservation, Room
+from users.models import Team
+
+User = get_user_model()
+
+
+class YourCommandTestCase(TestCase):
+    def setUp(self) -> None:
+        call_command("create_groups_and_permissions")
+        self.room = Room.objects.create(name="Room1", capacity=10)
+        self.team = Team.objects.create(name="Team")
+        self.user1 = User.objects.create_user(
+            username="user1", password="password", email="asfasf@asd.com", team=self.team
+        )
+        self.user2 = User.objects.create_user(username="user2", password="password", email="123@asd.com", phone="123")
+        self.reservation1 = Reservation.objects.create(
+            room=self.room,
+            reserver_user=self.user1,
+            team=self.team,
+            start_date=timezone.now() + timezone.timedelta(hours=1),
+            end_date=timezone.now() + timezone.timedelta(hours=3),
+        )
+        self.reservation2 = Reservation.objects.create(
+            room=self.room,
+            reserver_user=self.user1,
+            team=self.team,
+            start_date=timezone.now() + timezone.timedelta(hours=3),
+            end_date=timezone.now() + timezone.timedelta(hours=4),
+        )
+
+    def test_command_execution(self):
+        out = StringIO()
+        sys.stdout = out
+        call_command("reservation_email_reminder")
+        output = out.getvalue().strip()
+        sys.stdout = sys.__stdout__
+        self.assertIn("1 reminder email sent.", output)
